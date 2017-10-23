7336d1010909982c5fb0104f538f1dac259d9acc
Trevor Nelson <tsnelson86@gmail.com>
2017-10-22 23:51:01 -0600

    project -working version

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+  {
+    customerName: "Ahmed",
+    photo: "ahmed@example.com",
+    scores: [
+    	1,
+    	2,
+    	3,
+    	4,
+    	5,
+    	1,
+    	2,
+    	3,
+    	4,
+    	5
+    ]
+  }
+];
+
+module.exports = friendsArray;
