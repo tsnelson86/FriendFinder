e6d798cff7716631e077c51dd2c1d5f8a78c123c
Trevor Nelson <tsnelson86@gmail.com>
2017-10-23 19:44:06 -0600

    final

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,29 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res) {
     friendsData.push(req.body);
+    var match = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
+
+    var userData = req.body;
+    var userName = userData.name;
+    var userPhoto = userData.photo;
+    var userScores = userData.scores;
+    var scoreDiff = 0;
+
+    for  (var i=0; i < friendsData.length; i++) {
+      scoreDiff = 0;
+      for (var j = 0; j < friendsData[i].scores[j]; j++){
+        scoreDiff += Math.abs(parseInt(userScores[j]) - parseInt(friendsData[i].scores[j]));
+        if (scoreDiff <= match.friendDifference){
+          match.name = friendsData[i].customerName;
+          match.photo = friendsData[i].photo;
+          match.friendDifference = scoreDiff;
+        }
+      }
+    }
+    res.json(match);
   });
-  console.log(friendsData);
 };
